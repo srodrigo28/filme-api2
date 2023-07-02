a0389ff66b039891bbc85b05344d31e109c3801d
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2023-07-02 16:12:17 -0300

    aula 39 finalizada

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Header from './components/Header'
 import Home from './pages/Home'
 import Filme from './pages/Filme'
 
 function RouteApp() {
     return (
         <BrowserRouter>
+            <Header />
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/home" element={<Home />} />
